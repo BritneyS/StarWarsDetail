a360b31e4a0d37a4f4958ca16584343643a1cccd
Britney Smith <brit.smith7@gmail.com>
2018-10-25 10:23:58 -0400

    WIP - added Film object

---
+++ StarWarsDetail/StarWarsDetail/Model/Film.swift
@@ -0,0 +1,13 @@
+//
+//  Film.swift
+//  StarWarsDetail
+//
+//  Created by Britney Smith on 10/25/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import Foundation
+
+class Film: Codable {
+    let characters: [Characters]
+}
