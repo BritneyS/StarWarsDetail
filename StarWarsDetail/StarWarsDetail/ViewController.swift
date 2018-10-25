f7aec26263880a5f011364b5925a162e4d8f6a8f
Britney Smith <brit.smith7@gmail.com>
2018-10-25 10:13:19 -0400

    Initial commit: project created

---
+++ StarWarsDetail/StarWarsDetail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarsDetail
+//
+//  Created by Britney Smith on 10/25/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
