01f8bf2a231a8983314d2ee127c7dbaf0215dd2c
Britney Smith <brit.smith7@gmail.com>
2018-10-25 11:11:53 -0400

    Added CharacterListViewController

---
+++ StarWarsDetail/StarWarsDetail/Controllers/CharacterListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CharacterListViewController.swift
+//  StarWarsDetail
+//
+//  Created by Britney Smith on 10/25/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import UIKit
+
+class CharacterListViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    // MARK: Properties
+    
+    // MARK: Lifecyle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
