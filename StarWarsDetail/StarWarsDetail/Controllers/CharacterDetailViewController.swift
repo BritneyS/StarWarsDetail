46115ba9380f28078339d0050a7f3d1cf2894d3d
Britney Smith <brit.smith7@gmail.com>
2018-10-26 14:02:37 -0400

    Added CharacterDetailViewController

---
+++ StarWarsDetail/StarWarsDetail/Controllers/CharacterDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CharacterDetailViewController.swift
+//  StarWarsDetail
+//
+//  Created by Britney Smith on 10/26/18.
+//  Copyright © 2018 Britney Smith. All rights reserved.
+//
+
+import UIKit
+
+class CharacterDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
